ceacc6d8ca50ecfcdb19b663c308293c2e44d698
MaikeDsc <maquiavelcampos@gmail.com>
2025-08-19 10:12:56 -0400

    Horo e os corpos

--- Beecrowd/BEE 3479.py
+++ Beecrowd/BEE 3479.py
@@ -1,27 +1,73 @@
 dd, mm =  map(int, input().split('/'))
 
-if mm == 3:
-    if dd >= 21:
-        print('aries')
-    elif dd <= 20:
+if mm == 1:
+    if dd <= 19:
+        print('capricornio')
+    elif dd >= 20:
+        print('aquario')
+
+elif mm == 2:
+    if dd <= 18:
+        print('aquario')
+    elif dd >= 19:
         print('peixes')
 
-elif mm == 4: 
+if mm == 3:
     if dd <= 20:
         print('peixes')
-    if dd >=21:
+    elif dd >= 21:
+        print('aries')
+    
+elif mm == 4: 
+    if dd <= 20:
+        print('aries')
+    elif dd >=21:
         print('touro')
 
 elif mm ==  5:
     if dd <=20:
         print('touro')
-    if dd >= 21:
+    elif dd >= 21:
         print('gemeos')
 
-if mm == 6:
+elif mm == 6:
     if dd <= 20:
         print('gemeos')
-    if  dd >= 21:
+    elif dd >= 21:
         print('cancer')
 
+elif mm == 7:
+    if dd <= 22:
+        print('cancer')
+    elif dd >= 23:
+        print('leao')
 
+elif mm == 8:
+    if dd <= 22:
+        print('leao')
+    elif dd >= 23:
+        print('virgem')
+
+elif mm == 9:
+    if dd <= 22:
+        print('virgem')
+    elif dd >= 23:
+        print('libra')
+
+elif mm == 10:
+    if dd <= 22:
+        print('libra')
+    elif dd >= 23:
+        print('escorpiao')
+
+elif mm == 11:
+    if dd <= 21:
+        print('escorpiao')
+    elif dd >= 22:
+        print('sagitario')
+
+elif mm == 12:
+    if dd <= 21:
+        print('sagitario')
+    elif dd >= 22:
+        print('capricornio')
