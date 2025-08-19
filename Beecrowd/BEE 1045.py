79f210a0b192d0964e6de72728f87a47906af342
MaikeDsc <maquiavelcampos@gmail.com>
2025-08-19 15:28:23 -0400

    Tipos de triangulos

---
+++ Beecrowd/BEE 1045.py
@@ -0,0 +1,39 @@
+a, b, c = map(float, input().split())
+
+nums = [a,b,c]
+nums.sort()
+nums = nums[::-1]
+a = nums[0]
+b = nums[1]
+c = nums[2]
+
+if a >= (b+c):
+    print('NAO FORMA TRIANGULO')
+
+else:
+    if (a**2) == ((b**2) + (c**2)):
+        print('TRIANGULO RETANGULO')
+        if a == b == c:
+            print('TRIANGULO EQUILATERO')
+
+        elif (a == b) or (a == c) or (b == c):
+            print('TRIANGULO ISOSCELES') 
+
+
+    elif (a**2) > ((b**2) + (c**2)):
+        print('TRIANGULO OBTUSANGULO')
+
+        if a == b == c:
+            print('TRIANGULO EQUILATERO')
+
+        elif (a == b) or (a == c) or (b == c):
+            print('TRIANGULO ISOSCELES') 
+
+    elif (a**2) < ((b**2) + (c**2)):
+        print('TRIANGULO ACUTANGULO')
+
+        if a == b == c:
+            print('TRIANGULO EQUILATERO')
+
+        elif (a == b) or (a == c) or (b == c):
+            print('TRIANGULO ISOSCELES') 
